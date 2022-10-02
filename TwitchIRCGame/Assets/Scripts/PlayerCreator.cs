a8c9961543218e37d948b4da148e97d6150deb09
Iosif2510 <gpotatok@gmail.com>
2022-10-02 17:45:29 +0900

    Prototype Unity Project Init

---
+++ TwitchIRCGame/Assets/Scripts/PlayerCreator.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TwitchIRCGame
+{
+    public class PlayerCreator : MonoBehaviour
+    {
+        [SerializeField]
+        private TwitchIRC twitchIRC;
+        [SerializeField]
+        private int maxTeamPlayerCount;
+        [SerializeField]
+        private Player playerPrefab;
+
+        private int spawnCount = 0;
+        private List<Dictionary<string, Player>> playerTeams;
+        [SerializeField]
+        private Transform playerPositionParent;
+        private List<Transform> playerPositions;
+
+        // Start is called before the first frame update
+        void Awake()
+        {
+            playerTeams = new List<Dictionary<string, Player>>(2);
+            playerTeams.Add(new Dictionary<string, Player>(maxTeamPlayerCount));
+            playerTeams.Add(new Dictionary<string, Player>(maxTeamPlayerCount));
+            playerPositions = new List<Transform>();
+            foreach (Transform position in playerPositionParent)
+            {
+                playerPositions.Add(position);
+            }
+            twitchIRC.newChatMessageEvent.AddListener(NewMessageParse);
+        }
+
+        public void NewMessageParse(Chatter chatter)
+        {
+            if (playerTeams[0].ContainsKey(chatter.tags.userId) || playerTeams[1].ContainsKey(chatter.tags.userId))
+            {
+                //TODO action
+            }
+            else if (spawnCount < maxTeamPlayerCount * 2)
+            {
+                string[] message = chatter.message.Split(' ');
+                if (message[0] == "!join")
+                {
+                    Player newPlayer;
+                    spawnCount++;
+                    int teamIndex = int.Parse(message[1]) - 1;
+                    if ((teamIndex == 0) || (teamIndex == 1))
+                    {
+                        if (playerTeams[teamIndex].Count < maxTeamPlayerCount)
+                        {
+                            newPlayer = Instantiate(playerPrefab);
+                            playerTeams[teamIndex].Add(chatter.tags.userId, newPlayer);
+                            newPlayer.Name = chatter.tags.displayName;
+                            newPlayer.teamNumber = teamIndex + 1;
+                            newPlayer.transform.position = playerPositions[teamIndex * 4 + playerTeams[teamIndex].Count - 1].position;
+                        }
+                        else
+                        {
+                            Debug.Log($"Team {teamIndex + 1} full!");
+                        }
+                    }
+
+                }
+            }
+                
+            
+        }
+    }
+}
+
---
+++ TwitchIRCGame/Assets/Scripts/UIManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace TwitchIRCGame 
+{
+
+    public class UIManager : MonoBehaviour
+    {
+        [SerializeField]
+        public Camera mainCamera;
+
+        private static UIManager instance;
+        public static UIManager Instance
+        {
+            get
+            {
+                if (instance == null) return null;
+                return instance;
+            }
+        }
+
+        private void Awake()
+        {
+            if (instance == null)
+            {
+                instance = this;
+                DontDestroyOnLoad(gameObject);
+            }
+            else
+            {
+                if (instance != this) Destroy(gameObject);
+            }
+        }
+    }
+}
