7272501503a60dd0385d6eb90f04735e4200a62b
Indefyn <oud3noida@naver.com>
2022-11-09 00:36:18 +0900

    Convert comments into docstring
    CharacterAction.cs의 주석들을 도큐먼트로 변환함

--- TwitchIRCGame/Assets/Scripts/Action/CharacterAction.cs
+++ TwitchIRCGame/Assets/Scripts/Action/CharacterAction.cs
@@ -19,9 +19,29 @@ namespace TwitchIRCGame
         public string ActionName => actionName;
         public string Description => actionDescription;
 
-        public abstract bool IsTargeted { get; }        // 무대상 or 전체대상: false / 특정대상: true
-        public abstract bool IsTargetOpponent { get; }  // 아군대상: false / 적군대상: true
-        public abstract int ActionOrder { get; }        // ActionOrder 순으로 BattleManager에서 액션 시행
+        /// <summary>
+        /// 해당 행동의 대상 범위를 의미합니다.
+        /// <c>true</c>이면 특정 대상 행동,
+        /// <c>false</c>이면 대상이 없는 행동이거나 전체 대상 행동입니다.
+        /// <example>
+        /// 적 1명을 공격: <c>true</c><br/>
+        /// 아군 전체를 치유: <c>false</c><br/>
+        /// 도발: <c>false</c><br/>
+        /// </example>
+        /// </summary>
+        public abstract bool IsTargeted { get; }
+        
+        /// <summary>
+        /// 해당 행동이 어느 진영을 대상으로 하는지를 의미합니다.
+        /// <c>true</c>이면 적군 대상,
+        /// <c>false</c>이면 아군 대상 행동입니다.
+        /// </summary>
+        public abstract bool IsTargetOpponent { get; }
+        
+        /// <summary>
+        /// BattleManager에서 실행될 순서입니다.
+        /// </summary>
+        public abstract int ActionOrder { get; }
 
         public void DoAction()
         {
