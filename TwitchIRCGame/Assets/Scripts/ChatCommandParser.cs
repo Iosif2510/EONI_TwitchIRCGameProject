2166b51bfdfcc6788b75d99bb788f1dee8c6010c
yw9865 <yw9865@gmail.com>
2022-11-24 20:15:13 +0900

    join 일부 추가

--- TwitchIRCGame/Assets/Scripts/ChatCommandParser.cs
+++ TwitchIRCGame/Assets/Scripts/ChatCommandParser.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static TwitchIRCGame.Define;
+
 
 namespace TwitchIRCGame
 {
@@ -9,27 +11,111 @@ namespace TwitchIRCGame
         [SerializeField]
         private TwitchIRC twitchIRC;
 
+        //[SerializeField]
+        //private BattleManager battleManager = gameManager.Battle;
+        // 게임 매니저로 서번트 생성
+        // 배틀 매니저로 커맨드 생성
+
+
         private void Awake()
         {
             twitchIRC.newChatMessageEvent.AddListener(CommandParse);
         }
 
+        // gamemanager.createservant
+
+
+        // 여기서 직접 함수를 실행시키는 게 아닌
+        // 반대로 battlemanager에게 정보를 전달하는 형태로 만들어보자
+        // 응 싫어 퍼블릭으로 바꿀거야
+
+        //public void NewMessageParse(Chatter chatter)
+        //{
+        //    string[] message = chatter.message.Split(' ');
+        //    if (playerTeams[0].ContainsKey(chatter.tags.userId) || playerTeams[1].ContainsKey(chatter.tags.userId))
+        //    {
+        //        //TODO action
+        //        if (message[0] == "!leave")
+        //        {
+        //            PlayerCharacter deletePlayer;
+        //            string deleteId = chatter.tags.userId;
+        //            int containTeam = playerTeams[0].ContainsKey(deleteId) ? 0 : 1;
+        //            deletePlayer = playerTeams[containTeam][deleteId];
+        //            playerTeams[containTeam].Remove(deleteId);
+        //            Destroy(deletePlayer.gameObject);
+        //        }
+        //    }
+        //    else if (spawnCount < maxTeamPlayerCount * 2)
+        //    {
+        //        if ((message.Length == 2) && (message[0] == "!join"))
+        //        {
+        //            PlayerCharacter newPlayer;
+        //            spawnCount++;
+        //            if ((teamIndex == 0) || (teamIndex == 1))
+        //            {
+        //                if (playerTeams[teamIndex].Count < maxTeamPlayerCount)
+        //                {
+        //                    newPlayer = Instantiate(playerPrefab);
+        //                    playerTeams[teamIndex].Add(chatter.tags.userId, newPlayer);
+        //                    newPlayer.Name = chatter.tags.displayName;
+        //                    newPlayer.teamNumber = teamIndex + 1;
+        //                }
+        //                else
+        //                {
+        //                    Debug.Log($"Team {teamIndex + 1} full!");
+        //                }
+        //            }
+
+        //        }
+        //    }
+
+
+
         private void CommandParse(Chatter chatter)
         {
+            // 꽉 안찼으면 !join 가능
+            Debug.Log($"Action {GameManager.Instance.servantIDs.Count}");
+            if (GameManager.Instance.servantIDs.Count < GameManager.Battle.MaxServantNum)
+            {
+                if (chatter.message.Length == 5)
+                {
+                    // 조금이라도 채팅을 걸러보자
+                    string[] message = chatter.message.Split(' ');
+                    if (message[0] == "!join")
+                    {
+                        Debug.Log("Create Servant");
+                        GameManager.Instance.CreateServant(chatter.tags.userId);
+                        GameManager.Battle.servants[GameManager.Instance.servantIDs.Count].Name = chatter.tags.displayName;
+                    }
+                }
+                
+            }
+
+            // action chat
             if (IsPlayer(chatter.tags.userId))
             {
-                string[] command = chatter.message.Split(' ');
-                if (command[0][0] == '!')
+                string[] message = chatter.message.Split(' ');
+                // command chat
+                if (message[0][0] == '!')
                 {
-                    switch (command[0])
+                    switch (message[0])
                     {
                         case "!attack":
+                            Debug.Log("attack!");
+                            GameManager.Battle.SelectAction<Servant>(GameManager.Battle.servants, 0, 0);
+                            GameManager.Battle.SelectAction<Servant>(GameManager.Battle.servants, 1, 1, 1);
+                            GameManager.Battle.SelectAction<Servant>(GameManager.Battle.servants, 2, 1, 1);
                             break;
 
                     }
                 }
+                // idle chat
+                else
+                {
+
+                }
             }
-            
+
 
         }
 
